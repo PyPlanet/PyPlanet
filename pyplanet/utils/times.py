240b355ae10c8232309101a15fd650689695657d
Tom Valk <tomvalk@gmail.com>
2017-03-30 15:05:06 +0200

    [TASK] Add times utils, with the time format function (closes #31).

---
+++ pyplanet/utils/times.py
@@ -0,0 +1,22 @@
+import math
+
+
+def format_time(time):
+	"""
+	Format time from integer milliseconds to string format that could be displayed to the end-user.
+	:param time: Integer time in milliseconds.
+	:type time: int
+	:return: String output
+	:rtype: str
+	"""
+	hours = math.floor((time / 1000 / 60 / 60))
+	minutes = math.floor((time - (hours * 60 * 60 * 1000)) / 1000 / 60)
+	seconds = math.floor((time - (hours * 60 * 60 * 1000) - (minutes * 60 * 1000)) / 1000)
+	millis = (time - (hours * 60 * 60 * 1000) - (minutes * 60 * 1000) - (seconds * 1000))
+
+	formatted_time = ''
+	if hours > 0:
+		formatted_time += '{:02d}:{:02d}:'.format(hours, minutes)
+	else:
+		formatted_time += '{}:'.format(str(minutes))
+	return formatted_time + '{:02d}.{:03d}'.format(seconds, millis)
