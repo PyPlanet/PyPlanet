a8ba70e8448d2c6a12e0249435c9fe29538b87e0
Tom Valk <tomvalk@gmail.com>
2017-04-29 15:15:32 +0200

    [FEATURE] Add abstract prompt view.

--- pyplanet/views/generics/alert.py
+++ pyplanet/views/generics/alert.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from pyplanet.views import TemplateView
 
 
@@ -55,14 +57,13 @@ class AlertView(TemplateView):
 	)
 
 	def __init__(
-		self, message, title='', size='md', buttons=None, manager=None
+		self, message, size='md', buttons=None, manager=None, **data
 	):
 		"""
 		Create an AlertView instance.
 		
 		:param message: The message to display to the end-user, Use ``\\n`` for new lines. You can use symbols from FontAwesome
 						by using Unicode escaped strings.
-		:param title: Title of message, currently not implemented for any size!
 		:param size: Size to use, this parameter should be a string, and one of the following choices:
 					 'sm', 'md' or 'lg. Defaults to 'md'.
 		:param buttons: Buttons to display, Should be an array with dictionary which contain: name.
@@ -83,15 +84,117 @@ class AlertView(TemplateView):
 			buttons = [{'name': 'OK'}]
 
 		self.data = dict(
-			title=title,
 			message=message,
 			buttons=buttons,
 			sizes=sizes,
 		)
-		self.hide_click = True
+		self.data.update(data)
+
+	async def handle(self, player, action, values, **kwargs):
+		await self.close(player)
 
 	async def close(self, player, **kwargs):
 		"""
 		Close the alert.
 		"""
 		await self.hide(player_logins=[player.login])
+
+
+class PromptView(AlertView):
+	"""
+	The PromptView is like the AlertView but can ask for a text entry.
+	
+	The 3 sizes:
+	sm, md and lg.
+	
+	You can listen for the results of the players input with the ``wait_for_input()`` async handler (future).
+	Example:
+		
+	.. code-block:: python
+	
+		PromptView('Please enter your name')
+	
+	"""
+
+	template_name = 'core.views/generics/prompt.xml'
+
+	SIZES = dict(
+		sm={
+			'top__pos': '0 24',
+			'top__size': '126.5 8',
+			'box__size': '120 40',
+			'bottom__pos': '0 -20',
+			'bottom__size': '120 2',
+			'text__pos': '-47.5 11',
+			'text__size': '100 4',
+			'input__pos': '0 0',
+			'input__size': '100 7',
+			'button_0__pos__left': -32,
+			'button_0__pos__top': -12,
+			'button_1__pos__left': 32,
+			'button_1__pos__top': -12,
+		},
+		md={
+			'top__pos': '0 38',
+			'top__size': '156.5 8',
+			'box__size': '150 67',
+			'bottom__pos': '0 -34',
+			'bottom__size': '150 2',
+			'text__pos': '-67.5 11',
+			'text__size': '135 24',
+			'input__pos': '0 -10',
+			'input__size': '135 7',
+			'button_0__pos__left': -32,
+			'button_0__pos__top': -24,
+			'button_1__pos__left': 32,
+			'button_1__pos__top': -24,
+		},
+		lg={
+			'top__pos': '0 67',
+			'top__size': '206.5 8',
+			'box__size': '200 125',
+			'bottom__pos': '0 -62',
+			'bottom__size': '200 2',
+			'text__pos': '-92.5 11',
+			'text__size': '185 74',
+			'input__pos': '0 -40',
+			'input__size': '185 7',
+			'button_0__pos__left': -52,
+			'button_0__pos__top': -52,
+			'button_1__pos__left': 52,
+			'button_1__pos__top': -52,
+		},
+	)
+
+	def __init__(self, message, size='md', buttons=None, manager=None, default='', validator=None):
+		super().__init__(message, size, buttons, manager)
+
+		self.default = default
+		self.validator = validator or self.validate_input
+
+		self.input_future = asyncio.Future()
+
+	async def wait_for_input(self):
+		return await self.input_future
+
+	def validate_input(self, value):
+		if not value or len(value) == 0:
+			return False, 'Empty value given!'
+		return True, None
+
+	async def handle(self, player, action, values, **kwargs):
+		self.data['errors'] = ''
+		value = self.default
+		if 'prompt_value' in values:
+			value = values['prompt_value']
+
+		valid, message = self.validator(value)
+
+		if valid:
+			await self.close(player)
+			self.input_future.set_result(value)
+			self.input_future.done()
+			return
+
+		self.data['errors'] = message
+		await self.display([player.login])
