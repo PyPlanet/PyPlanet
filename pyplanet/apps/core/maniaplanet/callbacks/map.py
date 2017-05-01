cd0e46444b994fa3caf4748dd6454db096c0762f
Tom Valk <tomvalk@gmail.com>
2017-05-01 20:41:38 +0200

    [FEATURE] Add management of next script + next script settings/vars in the mode manager.

--- pyplanet/apps/core/maniaplanet/callbacks/map.py
+++ pyplanet/apps/core/maniaplanet/callbacks/map.py
@@ -7,7 +7,6 @@ async def handle_map_begin(source, signal, **kwargs):
 	map = await Controller.instance.map_manager.handle_map_change(source)
 	return dict(map=map)
 
-
 async def handle_map_end(source, signal, **kwargs):
 	# Retrieve and update map, return it to our callback listeners.
 	map = await Controller.instance.map_manager.handle_map_change(source)
@@ -20,6 +19,13 @@ async def handle_playlist_modified(source, signal, **kwargs):
 		source = dict()
 	source['maps_updated'] = updated
 	return source
+
+async def handle_map_start(source, signal, **kwargs):
+	# Get the map
+	map = await Controller.instance.map_manager.get_map(source['map']['uid'])
+	return dict(
+		time=source['time'], count=source['count'], restarted=source['restarted'], map=map,
+	)
 
 
 # We don't use scripted map events due to the information we get and the stability of the information structure.
@@ -42,4 +48,18 @@ playlist_modified = Callback(
 	namespace='maniaplanet',
 	code='playlist_modified',
 	target=handle_playlist_modified
+)
+
+map_start = Callback(
+	call='Script.Maniaplanet.StartMap_Start',
+	namespace='maniaplanet',
+	code='map_start',
+	target=handle_map_start
+)
+
+map_start__end = Callback(
+	call='Script.Maniaplanet.StartMap_End',
+	namespace='maniaplanet',
+	code='map_start',
+	target=handle_map_start
 )
--- pyplanet/contrib/mode/manager.py
+++ pyplanet/contrib/mode/manager.py
@@ -1,4 +1,5 @@
 from pyplanet.contrib import CoreContrib
+from pyplanet.contrib.command import Command
 
 
 class ModeManager(CoreContrib):
@@ -20,33 +21,69 @@ class ModeManager(CoreContrib):
 		self._instance = instance
 
 		self._current_script = None
+		self._next_script = None
+
+		self._next_settings_update = dict()
+		self._next_variables_update = dict()
 
 	async def on_start(self):
 		"""
 		Handle startup, just before the apps will start. We will make sure we are ready to get requests for permissions.
 		"""
-		self._current_script = await self.get_script_name()
+		self._current_script = await self.get_current_script()
 
-	async def get_script_name(self, refresh=False):
+		# Listeners.
+		self._instance.signal_manager.listen('maniaplanet:map_start', self._map_begin)
+
+	async def _map_begin(self, *args, **kwargs):
+		if len(self._next_settings_update.keys()) > 0:
+			await self.update_settings(self._next_settings_update)
+			self._next_settings_update = dict()
+		if len(self._next_variables_update.keys()) > 0:
+			await self.update_variables(self._next_variables_update)
+			self._next_variables_update = dict()
+		await self.get_current_script(refresh=True)
+
+	async def get_current_script(self, refresh=False):
 		"""
 		Get the current script name.
+		
+		:param refresh: Refresh from server.
 		"""
 		if refresh or not self._current_script:
-			self._current_script = await self._instance.gbx.execute('GetScriptName')
+			payload = await self._instance.gbx.execute('GetScriptName')
+			self._current_script = payload['CurrentValue']
+			if 'NextValue' in payload:
+				self._next_script = payload['NextValue']
 		return self._current_script
 
-	async def get_script_info(self):
+	async def get_next_script(self, refresh=False):
+		"""
+		Get the next script name.
+		
+		:param refresh: Refresh from server.
+		"""
+		if refresh or not self._current_script:
+			payload = await self._instance.gbx.execute('GetScriptName')
+			self._current_script = payload['CurrentValue']
+			if 'NextValue' in payload:
+				self._next_script = payload['NextValue']
+		return self._next_script
+
+	async def get_current_script_info(self):
 		"""
 		Get the script info as a structure containing: Name, CompatibleTypes, Description, Version and the settings available.
 		"""
 		return await self._instance.gbx.execute('GetModeScriptInfo')
 
-	async def set_script_name(self, name, instant=False):
-		if instant:
-			await self._instance.gbx.execute('SetModeScriptText', name)
-		else:
-			await self._instance.gbx.execute('SetScriptName', name)
-		self._current_script = name
+	async def set_next_script(self, name):
+		"""
+		Set the next played script name (after map restart/skip).
+		
+		:param name: Name
+		"""
+		await self._instance.gbx.execute('SetScriptName', name)
+		self._next_script = name
 
 	async def get_settings(self):
 		"""
@@ -65,6 +102,16 @@ class ModeManager(CoreContrib):
 		current_settings.update(update_dict)
 		await self._instance.gbx.execute('SetModeScriptSettings', current_settings)
 
+	async def update_next_settings(self, update_dict):
+		"""
+		Queue setting changes for the next script (that will be active after restart).
+		
+		:param update_dict: The dictionary with the partial updated keys and values.
+		"""
+		if not isinstance(self._next_settings_update, dict):
+			self._next_settings_update = dict()
+		self._next_settings_update.update(update_dict)
+
 	async def get_variables(self):
 		"""
 		Get the mode script variables.
@@ -81,3 +128,13 @@ class ModeManager(CoreContrib):
 		variables = await self.get_variables()
 		variables.update(update_dict)
 		await self._instance.gbx.execute('SetModeScriptVariables', variables)
+
+	async def update_next_variables(self, update_dict):
+		"""
+		Queue variable changes for the next script (that will be active after restart).
+		
+		:param update_dict: The dictionary with the partial updated keys and values.
+		"""
+		if not isinstance(self._next_variables_update, dict):
+			self._next_variables_update = dict()
+		self._next_variables_update.update(update_dict)
