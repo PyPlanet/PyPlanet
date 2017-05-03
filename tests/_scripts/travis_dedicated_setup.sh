934f4949dc962b1e784775d93f995e60c71b11f3
Tom Valk <tomvalk@gmail.com>
2017-05-03 18:41:55 +0200

    [TASK] Upgrade the dedicated in travis to the latest beta one.

--- tests/_scripts/travis_dedicated_setup.sh
+++ tests/_scripts/travis_dedicated_setup.sh
@@ -13,11 +13,15 @@ cd ../../
 # From now we are in the project root.
 
 # Download the latest dedicated server.
-wget http://files.maniaplanet.com/ManiaPlanet3Beta/ManiaPlanetBetaServer_latest.zip -O ./dedicated.zip
+wget http://files.v04.maniaplanet.com/server/ManiaplanetServer_2017-05-02.zip -O ./dedicated.zip
+# wget http://files.maniaplanet.com/ManiaPlanet3Beta/ManiaPlanetBetaServer_latest.zip -O ./dedicated.zip
 
 # Unzip the dedicated into the subdirectory.
 mkdir -p ./dedicated
 unzip dedicated.zip -d ./dedicated
+
+# Download titles.
+wget https://v4.live.maniaplanet.com/ingame/public/titles/download/TMStadium@nadeo.Title.Pack.gbx -O ./dedicated/UserData/Packs/TMCanyon@nadeo.Title.Pack.gbx
 
 # Prepare configuration files.
 cp tests/_scripts/travis/dedicated/dedicated_cfg.txt dedicated/UserData/Config/dedicated_cfg.txt
