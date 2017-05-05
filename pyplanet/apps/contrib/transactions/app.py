fb96dfb4cdaa9a7a940bf0c742d0c9dda454abcf
Max Klaversma <maxklaversma@gmail.com>
2017-05-05 22:06:17 +0200

    Handling different BillUpdated states, added amount check on admin pay.

--- pyplanet/apps/contrib/transactions/app.py
+++ pyplanet/apps/contrib/transactions/app.py
@@ -1,7 +1,12 @@
+import logging
+import math
+
 from pyplanet.apps.config import AppConfig
 from pyplanet.contrib.command import Command
 
 from pyplanet.apps.core.maniaplanet import callbacks as mp_signals
+
+logger = logging.getLogger(__name__)
 
 
 class Transactions(AppConfig):
@@ -13,6 +18,10 @@ class Transactions(AppConfig):
 		super().__init__(*args, **kwargs)
 
 		self.current_bills = []
+		self.public_appreciation = 100
+
+		print("on_init")
+		logger.debug("on_init")
 
 	async def on_start(self):
 		await self.instance.permission_manager.register('pay', 'Pay planets to players', app=self, min_level=3)
@@ -26,6 +35,9 @@ class Transactions(AppConfig):
 
 		# Register callback.
 		self.instance.signal_manager.listen(mp_signals.other.bill_updated, self.bill_updated)
+
+		print("on_start")
+		logger.debug("on_start")
 
 	async def display_planets(self, player, data, **kwargs):
 		planets = await self.instance.gbx.execute('GetServerPlanets')
@@ -44,15 +56,54 @@ class Transactions(AppConfig):
 	async def pay_to_player(self, player, data, **kwargs):
 		try:
 			amount = int(data.amount)
-			billid = await self.instance.gbx.execute('Pay', player.login, amount, 'Payment from the server')
-			self.current_bills.append(dict(bill=billid, player=player, amount=-amount))
+
+			planets = await self.instance.gbx.execute('GetServerPlanets')
+			if amount <= (planets - 2 - math.floor(amount * 0.05)):
+				billid = await self.instance.gbx.execute('Pay', data.login, amount, 'Payment from the server')
+				self.current_bills.append(dict(bill=billid, admin=player, player=data.login, amount=-amount))
+			else:
+				message = '$z$s$fff» $i$f00Insufficient balance for paying $fff{}$f00 ($fff{}$f00 inc. tax) planets, only got $fff{}$f00.'.format(amount, (amount + 2 + math.floor(amount * 0.05)), planets)
+				await self.instance.gbx.execute('ChatSendServerMessageToLogin', message, player.login)
 		except ValueError:
 			message = '$z$s$fff» $i$f00The amount should be a numeric value.'
 			await self.instance.gbx.execute('ChatSendServerMessageToLogin', message, player.login)
 
 	async def bill_updated(self, bill_id, state, state_name, transaction_id, **kwargs):
-		if bill_id in self.current_bills:
-			print('BillUpdated for BillId {} {} (TxId {})'.format(bill_id, state_name, transaction_id))
+		current_bill = next((x for x in self.current_bills if x['bill'] == bill_id), None)
+		if current_bill is not None:
+			print('BillUpdated for BillId {}: "{}" ({}) (TxId {})'.format(bill_id, state_name, state, transaction_id))
+			logger.debug('BillUpdated for BillId {}: "{}" ({}) (TxId {})'.format(bill_id, state_name, state, transaction_id))
+			if state == 4:
+				if current_bill['amount'] > 0:
+					if current_bill['amount'] > self.public_appreciation:
+						message = '$z$s$fff» $f0fWe received a donation of $fff{}$f0f planets from $fff{}$z$s$f0f. Thank You!'.format(current_bill['amount'], current_bill['player'].nickname)
+						await self.instance.gbx.execute('ChatSendServerMessage')
+					else:
+						message = '$z$s$fff» $f0fYou made a donation of $fff{}$f0f planets. Thank You!'.format(current_bill['amount'])
+						await self.instance.gbx.execute('ChatSendServerMessageToLogin', message, current_bill['player'].login)
+				else:
+					message = '$z$s$fff» $f0fPayment of $fff{}$f0f planets to $fff{}$f0f confirmed!'.format(current_bill['amount'], current_bill['player'])
+					await self.instance.gbx.execute('ChatSendServerMessageToLogin', message, current_bill['admin'].login)
+
+				self.current_bills.remove(current_bill)
+			elif state == 5:
+				if current_bill['amount'] > 0:
+					message = '$z$s$fff» $i$f00Transaction refused!'
+					await self.instance.gbx.execute('ChatSendServerMessageToLogin', message, current_bill['player'].login)
+				else:
+					message = '$z$s$fff» $i$f00Transaction refused!'
+					await self.instance.gbx.execute('ChatSendServerMessageToLogin', message, current_bill['admin'].login)
+
+				self.current_bills.remove(current_bill)
+			elif state == 6:
+				if current_bill['amount'] > 0:
+					message = '$z$s$fff» $i$f00Transaction failed: $fff{}$f00!'.format(state_name)
+					await self.instance.gbx.execute('ChatSendServerMessageToLogin', message, current_bill['player'].login)
+				else:
+					message = '$z$s$fff» $i$f00Transaction failed: $fff{}$f00!'.format(state_name)
+					await self.instance.gbx.execute('ChatSendServerMessageToLogin', message, current_bill['admin'].login)
+
+				self.current_bills.remove(current_bill)
 		else:
-			print('BillUpdated for unknown BillId {} {} (TxId {})'.format(bill_id, state_name, transaction_id))
-			print(self.current_bills)
+			print('BillUpdated for unknown BillId {}: "{}" ({}) (TxId {})'.format(bill_id, state_name, state, transaction_id))
+			logger.debug('BillUpdated for unknown BillId {}: "{}" ({}) (TxId {})'.format(bill_id, state_name, state, transaction_id))
