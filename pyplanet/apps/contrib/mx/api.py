0e08df6e98d618748999c262562a652fb7bddce3
Tom Valk <tomvalk@gmail.com>
2017-05-05 22:09:07 +0200

    [TASK] Add MX contrib app stub.

---
+++ pyplanet/apps/contrib/mx/api.py
@@ -0,0 +1,11 @@
+"""
+The MX API client class.
+"""
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class MXApi:
+	def __init__(self):
+		pass
---
+++ pyplanet/apps/contrib/mx/app.py
@@ -0,0 +1,6 @@
+from pyplanet.apps.config import AppConfig
+
+
+class MX(AppConfig):
+	name = 'pyplanet.apps.contrib.mx'
+	app_dependencies = ['core.maniaplanet']
