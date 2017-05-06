baaac9a26a2c509ee727a42c0046e67d8c63d984
Tom Valk <tomvalk@gmail.com>
2017-05-06 19:10:32 +0200

    [TASK] Add logo view to pyplanet core app.

--- pyplanet/apps/core/pyplanet/permissions.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-from pyplanet.core import Controller
-
-async def register_permissions(app):
-	perms = Controller.instance.permission_manager
-	await asyncio.gather(
-		perms.register('use_ok', 'Use the /admin ok (or //ok) command', app=app, min_level=2),
-	)
