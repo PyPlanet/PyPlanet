c078aff6c39f772b5622d166482233c1ee106e36
Tom Valk <tomvalk@gmail.com>
2017-05-18 15:41:17 +0200

    [FEATURE] Add endround, endwuround, and set repartition to tm flow admin.

--- pyplanet/apps/contrib/admin/__init__.py
+++ pyplanet/apps/contrib/admin/__init__.py
@@ -3,6 +3,7 @@ from pyplanet.apps.contrib.admin.map import MapAdmin
 from pyplanet.apps.contrib.admin.player import PlayerAdmin
 from pyplanet.apps.contrib.admin.server import ServerAdmin
 from pyplanet.apps.contrib.admin.pyplanet import PyPlanetAdmin
+from pyplanet.apps.contrib.admin.flow import FlowAdmin
 
 
 class Admin(AppConfig):
@@ -16,9 +17,11 @@ class Admin(AppConfig):
 		self.map = MapAdmin(self)
 		self.server = ServerAdmin(self)
 		self.pyplanet = PyPlanetAdmin(self)
+		self.flow = FlowAdmin(self)
 
 	async def on_start(self):
 		await self.player.on_start()
 		await self.map.on_start()
 		await self.server.on_start()
 		await self.pyplanet.on_start()
+		await self.flow.on_start()
