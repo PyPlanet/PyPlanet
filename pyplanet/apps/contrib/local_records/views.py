bb2b9fa1ff37d136b5537c85fd3a37b5fc170cd5
Max Klaversma <maxklaversma@gmail.com>
2017-05-20 11:27:01 +0200

    Fixed wrong startpoint for recordwidget when personal record in top entries.

--- pyplanet/apps/contrib/local_records/views.py
+++ pyplanet/apps/contrib/local_records/views.py
@@ -54,7 +54,7 @@ class LocalRecordsWidget(TimesWidgetView):
 			else:
 				if player_index <= self.top_entries:
 					# Player record is in top X, get following records (top entries + 1 onwards)
-					records += self.app.current_records[(self.top_entries + 1):(self.record_amount + 1)]
+					records += self.app.current_records[self.top_entries:self.record_amount]
 					custom_start_index = (self.top_entries + 1)
 				else:
 					# Player record is not in top X, get records around player record
