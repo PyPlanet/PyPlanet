84ac57e8d2fc1164624342eaca519d3f91ba4bc3
Tom Valk <tomvalk@gmail.com>
2017-05-30 19:14:11 +0200

    [TASK] Bump version.

--- pyplanet/__init__.py
+++ pyplanet/__init__.py
@@ -2,5 +2,5 @@
 PyPlanet, a robust and simple Maniaplanet Server Controller for Python 3.5+.
 Please see LICENSE file in root of project.
 """
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 __author__ = 'Tom Valk'
