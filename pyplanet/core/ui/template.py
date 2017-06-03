dae33ac281a4962f3d62a740fc6c88cddf097bd5
Tom Valk <tomvalk@gmail.com>
2017-06-03 12:30:37 +0200

    [TASK] Make sure we only use one environment.

--- pyplanet/core/ui/template.py
+++ pyplanet/core/ui/template.py
@@ -1,4 +1,4 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, select_autoescape
 
 from pyplanet.conf import settings
 from pyplanet.core.ui.loader import PyPlanetLoader
@@ -6,6 +6,23 @@ from pyplanet.core.ui.loader import PyPlanetLoader
 
 async def load_template(file):
 	return Template(file)
+
+
+class _EnvironmentManager:
+	def __init__(self):
+		self._environment = None
+
+	@property
+	def environment(self):
+		if not self._environment:
+			self._environment = Environment(
+				loader=PyPlanetLoader.get_loader(),
+				autoescape=select_autoescape(['html', 'xml', 'Txt', 'txt', 'ml', 'ms', 'script.txt', 'Script.Txt']),
+				auto_reload=bool(settings.DEBUG),
+			)
+		return self._environment
+
+EnvironmentManager = _EnvironmentManager()
 
 
 class Template:
@@ -25,13 +42,8 @@ class Template:
 	"""
 
 	def __init__(self, file):
-
 		self.file = file
-		self.env = Environment(
-			loader=PyPlanetLoader.get_loader(),
-			autoescape=select_autoescape(['html', 'xml', 'Txt', 'txt', 'ml', 'ms', 'script.txt', 'Script.Txt']),
-			auto_reload=bool(settings.DEBUG),
-		)
+		self.env = EnvironmentManager.environment
 		self.template = self.env.get_template(file)
 
 	async def render(self, **data):
