ecfda4bf2ef9314b258d3ecaa7d3eb4aa522570f
Tom Valk <tomvalk@gmail.com>
2017-06-03 16:20:47 +0200

    [FEATURE] Replace the nickname of the map info by the nickname on maniascripted client.

--- pyplanet/apps/contrib/info/views.py
+++ pyplanet/apps/contrib/info/views.py
@@ -1,5 +1,6 @@
 
 from pyplanet import __version__ as version
+from pyplanet.apps.core.maniaplanet.models import Player
 from pyplanet.views.generics.widget import WidgetView
 from pyplanet.utils import times
 from pyplanet.contrib.player.exceptions import PlayerNotFound
@@ -19,18 +20,11 @@ class MapInfoWidget(WidgetView):
 
 	async def get_context_data(self):
 		map = self.app.instance.map_manager.current_map
-		map_author = map.author_login
-		try:
-			author = await self.app.instance.player_manager.get_player(map.author_login)
-			map_author = author.nickname
-		except PlayerNotFound:
-			if map.author_nickname:
-				map_author = map.author_nickname
 
 		context = await super().get_context_data()
 		context.update({
 			'map_name': map.name,
-			'map_author': map_author,
+			'map_author': map.author_login,
 			'map_authortime': times.format_time(map.time_author) if map.time_author and map.time_author > 0 else '-',
 			'map_environment': map.environment,
 		})
