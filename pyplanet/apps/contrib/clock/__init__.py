db29886700ef0244fec8135361624bea31d1ef8a
Simon Keil <simon.keil@gmx.de>
2017-10-06 17:05:29 +0200

    Converted signals to the new 0.5.0 registering style (clock).

--- pyplanet/apps/contrib/clock/__init__.py
+++ pyplanet/apps/contrib/clock/__init__.py
@@ -17,9 +17,9 @@ class Clock(AppConfig):
         self.widget = ClockWidget(self)
 
     async def on_start(self):
-        self.instance.signal_manager.listen(mp_signals.player.player_connect, self.player_connect)
-        self.instance.signal_manager.listen(mp_signals.map.map_start, self.map_start)
-        self.instance.signal_manager.listen(pyplanet_start_after, self.on_after_start)
+        self.context.signals.listen(mp_signals.player.player_connect, self.player_connect)
+        self.context.signals.listen(mp_signals.map.map_start, self.map_start)
+        self.context.signals.listen(pyplanet_start_after, self.on_after_start)
 
     async def player_connect(self, player, **kwargs):
         await self.widget.display(player)
