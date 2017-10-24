cd14dccf3a4603a162b6b85a51ccb251e3950545
Tom Valk <tomvalk@gmail.com>
2017-10-24 11:18:44 +0200

    [TASK] Apply some improvements to the warn command logic.

--- pyplanet/apps/contrib/admin/player.py
+++ pyplanet/apps/contrib/admin/player.py
@@ -1,6 +1,8 @@
 """
 Player Admin methods and functions.
 """
+import asyncio
+
 from pyplanet.contrib.command import Command
 from pyplanet.contrib.player.exceptions import PlayerNotFound
 from pyplanet.views.generics.alert import show_alert
@@ -242,9 +244,16 @@ class PlayerAdmin:
 
 	async def warn_player(self, player, data, **kwargs):
 		try:
-			warn_player = await self.instance.player_manager.get_player(login=data.login)
-			await show_alert(warn_player, 'You have just been warned! Ask the present admin for further information and / or potential consequences.', size='sm', buttons=None)
-			await self.instance.chat('$i$ff0 Player {} $i$ff0has received a warning'.format(warn_player.nickname), player)
+			warn_player = await self.instance.player_manager.get_player(login=data.login, lock=False)
+
+			await asyncio.gather(
+				show_alert(
+					warn_player,
+					'You have just been warned! Ask the present admin for further information and / or potential consequences.',
+					size='sm', buttons=None
+				),
+				self.instance.chat('$i$ff0 Player {} $z$s$i$ff0has received a warning'.format(warn_player.nickname), player)
+			)
 		except PlayerNotFound:
 			message = '$i$f00Unknown login!'
 			await self.instance.chat(message, player.login)
