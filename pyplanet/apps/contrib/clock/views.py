83e0df05fb3e53c839e0131b87a6e115963a5dc0
Tom Valk <tomvalk@gmail.com>
2017-11-07 10:24:01 +0100

    [TASK] Move around the clock and logo.

--- pyplanet/apps/contrib/clock/views.py
+++ pyplanet/apps/contrib/clock/views.py
@@ -3,7 +3,7 @@ from pyplanet.views.generics.widget import WidgetView
 
 class ClockWidget(WidgetView):
     widget_x = 150
-    widget_y = -50
+    widget_y = -54
     template_name = 'clock/clock.xml'
 
     def __init__(self, app):
@@ -11,4 +11,3 @@ class ClockWidget(WidgetView):
         self.app = app
         self.manager = app.context.ui
         self.id = 'pyplanet__widgets_clock'
-
