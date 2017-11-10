ec7d105c7633119b8b98413ed76e8a5b334f3007
Max Klaversma <maxklaversma@gmail.com>
2017-11-10 17:18:25 +0100

    Added vote pass admin command.

--- pyplanet/apps/contrib/voting/__init__.py
+++ pyplanet/apps/contrib/voting/__init__.py
@@ -60,9 +60,11 @@ class Voting(AppConfig):
 											self.setting_enabled_skip)
 
 		await self.instance.permission_manager.register('cancel', 'Cancel the current vote', app=self, min_level=1)
+		await self.instance.permission_manager.register('pass', 'Pass the current vote', app=self, min_level=1)
 
 		await self.instance.command_manager.register(
 			Command(command='cancel', target=self.cancel_vote, perms='voting:cancel', admin=True),
+			Command(command='pass', target=self.pass_vote, perms='voting:pass', admin=True),
 			Command(command='y', aliases=['yes'], target=self.vote_yes),
 			Command(command='n', aliases=['no'], target=self.vote_no),
 			Command(command='replay', target=self.vote_replay),
@@ -109,10 +111,23 @@ class Voting(AppConfig):
 			await self.instance.chat(message, player)
 			return
 
+		message = '$0cfAdmin $fff{}$z$s$0cf cancelled the current vote to $fff{}$z$s$0cf.'.format(player.nickname, self.current_vote.action)
+		await self.instance.chat(message)
+
 		self.current_vote = None
 
-		message = '$0cfAdmin $fff{}$z$s$0cf cancelled the current vote.'.format(player.nickname)
+	async def pass_vote(self, player, data, **kwargs):
+		if self.current_vote is None:
+			message = '$i$f00There is currently no vote in progress.'
+			await self.instance.chat(message, player)
+			return
+
+		await self.current_vote.fire_finished_event()
+
+		message = '$0cfAdmin $fff{}$z$s$0cf passed the current vote to $fff{}$z$s$0cf.'.format(player.nickname, self.current_vote.action)
 		await self.instance.chat(message)
+
+		self.current_vote = None
 
 	async def vote_added(self, vote, player):
 		if vote.votes_required == len(vote.votes_current):
@@ -353,7 +368,7 @@ class Voting(AppConfig):
 			needed_votes += 1
 		if needed_votes > self.instance.player_manager.count_players:
 			needed_votes = self.instance.player_manager.count_players
-		new_vote.votes_required = needed_votes
+		new_vote.votes_required = needed_votes + 2
 		new_vote.vote_added = self.vote_added
 		new_vote.vote_removed = self.vote_removed
 		new_vote.vote_finished = finished_event
