70eebcc32cc6a8426db657b684877c7f4c861a8e
Tom Valk <tomvalk@gmail.com>
2018-01-13 14:42:11 +0100

    [TASK] Don't silence migration errors in the karma migration.

--- pyplanet/apps/contrib/karma/migrations/000_add_expanded_score.py
+++ pyplanet/apps/contrib/karma/migrations/000_add_expanded_score.py
@@ -6,15 +6,10 @@ from ..models.karma import Karma
 
 def upgrade(migrator: SchemaMigrator):
 	expanded_score_field = FloatField(null=True)
-
-	try:
-		migrate(
-			migrator.drop_not_null(Karma._meta.db_table, 'score'),
-			migrator.add_column(Karma._meta.db_table, 'expanded_score', expanded_score_field),
-		)
-	except Exception as e:
-		print(str(e))
-		print('Migration failed but silencing error!')
+	migrate(
+		migrator.drop_not_null(Karma._meta.db_table, 'score'),
+		migrator.add_column(Karma._meta.db_table, 'expanded_score', expanded_score_field),
+	)
 
 
 def downgrade(migrator: SchemaMigrator):
