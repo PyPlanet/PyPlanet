ba64dd31907ac2559dcece66a5700c5ad1ea52cc
Simon Keil <simon.keil@gmx.de>
2018-02-02 20:16:17 +0100

    Logo now has the correct target URL. Basic error handling for the URL and ID settings.

--- pyplanet/apps/contrib/discord/__init__.py
+++ pyplanet/apps/contrib/discord/__init__.py
@@ -1,6 +1,6 @@
 import json
 import urllib.request as req
-import urllib
+import urllib.error
 import logging
 from pyplanet.apps.config import AppConfig
 from pyplanet.apps.contrib.discord.view import DiscordLogoView
@@ -15,7 +15,7 @@ class Discord(AppConfig):
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.Logo = DiscordLogoView(manager=self.context.ui)
+		self.Logo = DiscordLogoView(self, manager=self.context.ui)
 		self.join_url = None
 		self.server_id = None
 
@@ -41,21 +41,42 @@ class Discord(AppConfig):
 			self.setting_discord_join_url, self.setting_discord_server_id
 		)
 		await self.reload_settings()
-		self.Logo = DiscordLogoView(manager=self.context.ui)
+		self.Logo = DiscordLogoView(self, manager=self.context.ui)
 		await self.Logo.display()
 
 	async def on_connect(self, player, **kwargs):
 		await self.Logo.display(player_logins=[player.login])
 
 	async def reload_settings(self, *args, **kwargs):
-		self.join_url = await self.setting_discord_join_url.get_value(refresh=True)
-		self.server_id = await self.setting_discord_server_id.get_value(refresh=True)
+		url_setting = await self.setting_discord_join_url.get_value(refresh=True)
+		id_setting = await self.setting_discord_server_id.get_value(refresh=True)
+		url_validity = await self.is_url_valid(url_setting)
+		if url_validity:
+			self.join_url = url_setting
+		else:
+			await self.instance.chat('$f00$iInvalid Discord Invite URL')
+		id_validity = await self.is_url_valid("https://discordapp.com/api/guilds/" + id_setting + "/widget.json")
+		if id_validity:
+			self.server_id = id_setting
+		else:
+			await self.instance.chat('$f00$iInvalid Discord Server ID')
+		await self.Logo.display()
+
+	async def is_url_valid(self, url):
+		try:
+			request = urllib.request.Request(url)
+			request.add_header('User-Agent', "PyPlanet")
+			logging.debug("URL WAS CHECKED")
+			req.urlopen(request).read()
+			return True
+		except Exception as e:
+			logging.error(e)
+			return False
 
 	async def get_online_users(self):
 		url = "https://discordapp.com/api/guilds/" + self.server_id + "/widget.json"
 		request = urllib.request.Request(url)
 		request.add_header('User-Agent', "PyPlanet")
-		logging.debug(url)
 		data = json.loads(req.urlopen(request).read())
 		non_bot_users = []
 		bots = []
--- pyplanet/apps/contrib/discord/view.py
+++ pyplanet/apps/contrib/discord/view.py
@@ -8,12 +8,16 @@ class DiscordLogoView(TemplateView):
 	size_y = 50
 	template_name = 'discord/discord_logo.xml'
 
-	def __init__(self, *args, **kwargs):
+	def __init__(self, app, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		self.app = app
 		self.id = 'discord__logo'
 
 	async def get_context_data(self):
 		context = await super().get_context_data()
+		context.update({
+			'discord_url': self.app.join_url
+		})
 		return context
 
 	async def display(self, **kwargs):
