1bf148f95746df8c92dc60d23f0209d8993075bb
Tom Valk <tomvalk@gmail.com>
2019-02-17 12:15:01 +0200

    [TASK] Implement improvements and convert spaces to tabs in the mapbrowser.

--- pyplanet/apps/contrib/admin/mapbrowser.py
+++ pyplanet/apps/contrib/admin/mapbrowser.py
@@ -1,26 +1,22 @@
-import logging
-
-from pyplanet.apps.config import AppConfig
 from pyplanet.contrib.command import Command
-from pyplanet.contrib.permission import PermissionManager
-from pyplanet.contrib.command import CommandManager
-from pyplanet.contrib.chat import ChatManager
-from pyplanet.contrib.chat.query import ChatQuery
 from pyplanet.apps.contrib.admin.views.mapbrowser import BrowserView
 
 
 class MapBrowser:
-    def __init__(self, app):
-        self.app = app
-        self.instance = app.instance
+	def __init__(self, app):
+		self.app = app
+		self.instance = app.instance
 
-    async def on_start(self):
-        await self.instance.permission_manager.register('localmaps', 'Shows a file browser to browse the local files', app=self.app, min_level=3)
+	async def on_start(self):
+		await self.instance.permission_manager.register(
+			'localmaps', 'Shows a file browser to browse the local files',
+			app=self.app, min_level=3
+		)
 
-        await self.app.instance.command_manager.register(
-            Command(command='localmaps', target=self.show_browser, perms='admin:localmaps', admin=True)
-        )
+		await self.app.instance.command_manager.register(
+			Command(command='localmaps', target=self.show_browser, perms='admin:localmaps', admin=True)
+		)
 
-    async def show_browser(self, player, data, **kwargs):
-        view = BrowserView(self.app, player)
-        await view.set_dir('')
+	async def show_browser(self, player, data, **kwargs):
+		view = BrowserView(self.app, player)
+		await view.set_dir('')
