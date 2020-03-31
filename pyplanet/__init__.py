8e0af446e547c5765ab3c4949496ff0fd07bb155
Tom Valk <tomvalk@gmail.com>
2020-03-31 21:21:50 +0300

    [TASK] Change testing and other parameters for python versions.

--- pyplanet/__init__.py
+++ pyplanet/__init__.py
@@ -1,5 +1,5 @@
 """
-PyPlanet, a robust and simple Maniaplanet Server Controller for Python 3.5+.
+PyPlanet, a robust and simple Maniaplanet Server Controller for Python 3.6+.
 Please see LICENSE file in root of project.
 """
 __version__ = '0.7.4'
