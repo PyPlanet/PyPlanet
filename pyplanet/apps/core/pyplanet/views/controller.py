588b08e0e52b534793b21f4f8d292db92da0c433
Tom Valk <tomvalk@gmail.com>
2020-05-13 12:00:07 +0300

    [BUGFIX] Fixing auto-merging removal of the F9 feature.

--- pyplanet/apps/core/pyplanet/views/controller.py
+++ pyplanet/apps/core/pyplanet/views/controller.py
@@ -8,6 +8,8 @@ class ControllerView(TemplateView):
 		super().__init__(*args, **kwargs)
 		self.id = 'pyplanet__controller'
 
+		self.subscribe('f8', self.action_f8)
+
 	async def get_context_data(self):
 		from pyplanet.core import Controller
 		context = await super().get_context_data()
@@ -21,3 +23,8 @@ class ControllerView(TemplateView):
 
 	async def display(self, **kwargs):
 		return await super().display(**kwargs)
+
+	async def action_f8(self, player, *args, **kwargs):
+		await self.manager.instance.chat(
+			'$ff0Toggling visibility. You can show/hide with F8, and show/hide when driving with F9', player
+		)
