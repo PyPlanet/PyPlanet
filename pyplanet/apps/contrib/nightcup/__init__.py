e202ebb1f5943c1c80464e375afcefc58082c3fa
JosVroegindeweij <jos.vroegindeweij@hotmail.com>
2020-05-27 01:33:07 +0200

    Added resetting of properties after nightcup has finished. Not tested.

--- pyplanet/apps/contrib/nightcup/__init__.py
+++ pyplanet/apps/contrib/nightcup/__init__.py
@@ -486,23 +486,21 @@ class NightCup(AppConfig):
 							   f'has been removed from the qualified list')
 
 	async def set_ui_elements(self):
+		await self.set_properties()
+		await self.disable_standings_uis()
+		await self.move_dedi_ui()
+
+	async def set_properties(self):
 		try:
 			await self.instance.ui_manager.app_managers['core.pyplanet'].manialinks['pyplanet__controller'].hide()
+		except UIPropertyDoesNotExist:
+			logging.error('Something went wrong while moving the pyplanet logo.')
+		try:
 			await self.instance.ui_manager.app_managers['clock'].manialinks['pyplanet__widgets_clock'].hide()
 		except UIPropertyDoesNotExist:
-			logging.error('Something went wrong while moving pyplanet core widgets.')
+			logging.error('Something went wrong while moving the pyplanet clock.')
 
-		properties = ['countdown', 'personal_best_and_rank', 'position']
-		for p in properties:
-			pos = [float(c) for c in self.instance.ui_manager.properties.get_attribute(p, 'pos').split()]
-			pos[1] -= 20
-			pos = ' '.join([str(c) for c in pos])
-			self.instance.ui_manager.properties.set_attribute(p, 'pos', pos)
-		await self.instance.ui_manager.properties.send_properties()
-
-		await self.disable_standings_uis()
-
-		await self.move_dedi_ui()
+		await self.move_properties(-20)
 
 	async def move_dedi_ui(self):
 		for app in self.instance.ui_manager.app_managers.values():
@@ -533,8 +531,34 @@ class NightCup(AppConfig):
 			del app
 
 	async def reset_ui_elements(self):
+		await self.reset_properties()
 		await self.reset_dedi_ui()
 		await self.reset_standings_uis()
+
+	async def reset_properties(self):
+		try:
+			await self.instance.ui_manager.app_managers['core.pyplanet'].manialinks['pyplanet__controller'].show()
+		except UIPropertyDoesNotExist:
+			logging.error('Something went wrong while moving the pyplanet logo.')
+		try:
+			await self.instance.ui_manager.app_managers['clock'].manialinks['pyplanet__widgets_clock'].show()
+		except UIPropertyDoesNotExist:
+			logging.error('Something went wrong while moving the pyplanet clock.')
+
+		await self.move_properties(20)
+
+
+
+	async def move_properties(self, offset):
+		properties = ['countdown', 'personal_best_and_rank', 'position']
+		for p in properties:
+			pos = [float(c) for c in self.instance.ui_manager.properties.get_attribute(p, 'pos').split()]
+			pos[1] += 20
+			pos = ' '.join([str(c) for c in pos])
+			self.instance.ui_manager.properties.set_attribute(p, 'pos', pos)
+
+		# Update properties for every player
+		await self.instance.ui_manager.properties.send_properties()
 
 	async def reset_dedi_ui(self):
 		for app in self.instance.ui_manager.app_managers.values():
