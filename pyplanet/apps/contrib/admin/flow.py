8b66833b5b0d7bde7bc3e6c1810528e908f8ecd1
W1lla van den Munckhof <w1llaopgezwolle@gmail.com>
2021-02-05 20:40:18 +0100

    [Command] //pd smurfs for example
    Smurfs point distrubition as example and the rest for Xaseco rpoints commands.

--- pyplanet/apps/contrib/admin/flow.py
+++ pyplanet/apps/contrib/admin/flow.py
@@ -15,6 +15,8 @@ class FlowAdmin:
 
 	async def on_start(self):
 		await self.instance.permission_manager.register('end_round', 'Force ending a round (warmup, race or custom)', app=self.app, min_level=2)
+		await self.instance.permission_manager.register('player_points', 'Change the player points', app=self.app, min_level=2)
+		await self.instance.permission_manager.register('team_points', 'Change the team points', app=self.app, min_level=2)
 		await self.instance.permission_manager.register('points_repartition', 'Change the points repartition', app=self.app, min_level=2)
 
 		# Trackmania specific:
@@ -29,11 +31,31 @@ class FlowAdmin:
 				Command(command='pointsrepartition', aliases=['pointsrep'], target=self.set_point_repartition,
 						perms='admin:points_repartition', admin=True, description='Alters the points repartitioning.')
 					.add_param('repartition', nargs='*', type=str, required=True, help='Repartition, comma or space separated.'),
-			)
+				Command(command='playerpoints', aliases=['playerpoints'], target=self.set_player_points,
+						perms='admin:player_points', admin=True, description='Alters the Players Points for Round, Map, Match.')
+						.add_param(name='login', required=True)
+						.add_param(name='points', nargs='*', type=str, required=True, help='Repartition, comma or space separated.'),
+				Command(command='teampoints', aliases=['teampoints'], target=self.set_team_points,
+						perms='admin:team_points', admin=True, description='Alters the Teams Points for Round, Map, Match.')
+						.add_param(name='teamid', required=True)
+						.add_param(name='points', nargs='*', type=str, required=True, help='Repartition, comma or space separated.'),
+				Command(command='pd', target=self.set_pd_name,
+						perms='admin:points_repartition', admin=True, description='Alters the points repartitioning.')
+					.add_param('name', nargs='*', type=str, required=True, help='Repartition, comma or space separated.'),
+				)
 
 		# Shootmania specific.
 		if self.instance.game.game == 'sm':
-			pass
+			await self.instance.command_manager.register(
+				Command(command='playerpoints', aliases=['playerpoints'], target=self.set_player_points,
+						perms='admin:player_points', admin=True, description='Alters the Players Points for Round, Map, Match.')
+						.add_param(name='login', required=True)
+						.add_param(name='points', nargs='*', type=str, required=True, help='Repartition, comma or space separated.'),
+				Command(command='teampoints', aliases=['teampoints'], target=self.set_team_points,
+						perms='admin:team_points', admin=True, description='Alters the Teams Points for Round, Map, Match.')
+						.add_param(name='teamid', required=True)
+						.add_param(name='points', nargs='*', type=str, required=True, help='Repartition, comma or space separated.'),
+				)
 
 	async def end_round(self, player, data, **kwargs):
 		await self.instance.gbx.multicall(
@@ -52,7 +74,142 @@ class FlowAdmin:
 			self.instance.gbx('Trackmania.WarmUp.ForceStop', encode_json=False, response_id=False),
 			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has forced the WarmUp to an end.'.format(player.nickname))
 		)
-
+	
+	async def set_pd_name(self, player, data, **kwargs):
+		pd = data.name
+		
+		if 'smurfs' in pd:
+			points_smurfs = str('50,45,41,38,36,34,32,30,28,26,24,22,20,18,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,1,1').split(',')
+			partition = [str(p).strip() for p in points_smurfs]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, '$06fSmurfs Cup')
+			)
+		)
+		
+		if 'f1old' in pd:
+			points_f1old = str('10,8,6,5,4,3,2,1').split(',')
+			partition = [str(p).strip() for p in points_f1old]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'F1 Old')
+			)
+		)
+		
+		if 'f1new' in pd:
+			points_f1new = str('25,18,15,12,10,8,6,4,2,1').split(',')
+			partition = [str(p).strip() for p in points_f1new]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'F1 New')
+			)
+		)
+		
+		if 'motogp' in pd:
+			points_motogp = str('25,20,16,13,11,10,9,8,7,6,5,4,3,2,1').split(',')
+			partition = [str(p).strip() for p in points_motogp]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'MotoGP')
+			)
+		)
+		
+		if 'motogp5' in pd:
+			points_motogp5 = str('30,25,21,18,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1').split(',')
+			partition = [str(p).strip() for p in points_motogp5]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'MotoGP + 5')
+			)
+		)
+		
+		if 'fet1' in pd:
+			points_fet1 = str('12,10,9,8,7,6,5,4,4,3,3,3,2,2,2,1').split(',')
+			partition = [str(p).strip() for p in points_fet1]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'Formula ET Season 1')
+			)
+		)
+		
+		if 'fet2' in pd:
+			points_fet2 = str('15,12,11,10,9,8,7,6,6,5,5,4,4,3,3,3,2,2,2,1').split(',')
+			partition = [str(p).strip() for p in points_fet2]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'Formula ET Season 2')
+			)
+		)
+		
+		if 'fet3' in pd:
+			points_fet3 = str('15,12,11,10,9,8,7,6,6,5,5,4,4,3,3,3,2,2,2,2,1').split(',')
+			partition = [str(p).strip() for p in points_fet3]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'Formula ET Season 3')
+			)
+		)
+		
+		if 'champcar' in pd:
+			points_champcar = str('31,27,25,23,21,19,17,15,13,11,10,9,8,7,6,5,4,3,2,1').split(',')
+			partition = [str(p).strip() for p in points_champcar]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'Champ Car World Series')
+			)
+		)
+		
+		if 'superstars' in pd:
+			points_superstars = str('20,15,12,10,8,6,4,3,2,1').split(',')
+			partition = [str(p).strip() for p in points_superstars]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'Superstars')
+			)
+		)
+		
+		if 'simple5' in pd:
+			points_simple5 = str('5,4,3,2,1').split(',')
+			partition = [str(p).strip() for p in points_simple5]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'Simple 5')
+			)
+		)
+		
+		if 'simple10' in pd:
+			points_simple10 = str('10,9,8,7,6,5,4,3,2,1').split(',')
+			partition = [str(p).strip() for p in points_simple10]
+			
+			await self.instance.gbx.multicall(
+			self.instance.gbx('Trackmania.SetPointsRepartition', *partition, encode_json=False, response_id=False),
+			self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points repartition to: {}.'.format(
+				player.nickname, 'Simple 10')
+			)
+		)
+		
 	async def set_point_repartition(self, player, data, **kwargs):
 		partition = data.repartition
 
@@ -66,3 +223,40 @@ class FlowAdmin:
 				player.nickname, ','.join(partition))
 			)
 		)
+		
+	async def set_player_points(self, player, data, **kwargs):
+		login = data.login
+		points = data.points
+		if len(points) == 1:
+			points = str(points[0]).split(',')
+		points = [str(p).strip() for p in points]
+		
+		#login, 'RoundPoints', 'Mappoints', 'Matchpoints' for Sending/Updating PlayerPoints
+		if self.instance.game.game == 'sm':
+			method_playerpoints = 'Shootmania.SetPlayerPoints'
+		if self.instance.game.game in ['tm', 'tmnext']:
+			method_playerpoints = 'Trackmania.SetPlayerPoints'
+		await self.instance.gbx.multicall(
+				self.instance.gbx(method_playerpoints, login, *points, encode_json=False, response_id=False),
+				self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points distribution for Player: $fff{} $z$s$ff0 to: {}'.format(
+				player.nickname, login, points)
+			))
+			
+	async def set_team_points(self, player, data, **kwargs):
+		teamid = data.teamid
+		points = data.points
+		if len(points) == 1:
+			points = str(points[0]).split(',')
+		points = [str(p).strip() for p in points]
+		
+		#TeamId, 'RoundPoints', 'Mappoints', 'Matchpoints' for Sending/Updating TeamPoints
+		#TeamId = 0 (Blue) or 1 (Red)
+		if self.instance.game.game == 'sm':
+			method_teampoints = 'Shootmania.SetTeamPoints'
+		if self.instance.game.game in ['tm', 'tmnext']:
+			method_teampoints = 'Trackmania.SeTeamPoints'
+		await self.instance.gbx.multicall(
+				self.instance.gbx(method_teampoints, teamid, *points, encode_json=False, response_id=False),
+				self.instance.chat('$ff0Admin $fff{}$z$s$ff0 has changed the points distribution for Team: $fff{} $z$s$ff0 to: {}'.format(
+				player.nickname, teamid, points)
+			))
