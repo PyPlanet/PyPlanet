cb4b094d4b60087295e20bf9a6108db6972c61f6
Max Klaversma <maxklaversma@gmail.com>
2022-09-15 21:42:39 +0200

    Implemented downgrade in mapfolder visibility database migration.

--- pyplanet/apps/contrib/jukebox/migrations/001_map_folder_change_public_to_visibility.py
+++ pyplanet/apps/contrib/jukebox/migrations/001_map_folder_change_public_to_visibility.py
@@ -1,4 +1,4 @@
-from peewee import RawQuery
+from peewee import RawQuery, BooleanField
 from playhouse.migrate import migrate, SchemaMigrator
 
 from ..models.mapfolder import MapFolder
@@ -20,4 +20,16 @@ def upgrade(migrator: SchemaMigrator):
 		print('Migration failed but silencing error!')
 
 def downgrade(migrator: SchemaMigrator):
-	pass
+	try:
+		query = RawQuery(MapFolder, """SELECT * FROM `mapfolder` WHERE visibility = 'public'""")
+		public_folders = query.execute()
+
+		migrate(
+			migrator.add_column(MapFolder._meta.db_table, 'public', BooleanField()),
+			migrator.drop_column(MapFolder._meta.db_table, 'visibility')
+		)
+
+		MapFolder.update(public=True).where(MapFolder.id << [f.id for f in public_folders]).execute()
+	except Exception as e:
+		print(str(e))
+		print('Migration failed but silencing error!')
