0fc4654982b019c77df85866049ed38ab0ca6348
Max Klaversma <maxklaversma@gmail.com>
2023-08-27 11:03:44 +0200

    Added Race Rankings widget to the Live Rankings app.

--- pyplanet/apps/contrib/live_rankings/views.py
+++ pyplanet/apps/contrib/live_rankings/views.py
@@ -169,3 +169,131 @@ class LiveRankingsWidget(TimesWidgetView):
 			for player in self.app.instance.player_manager.online:
 				data[player.login] = dict(times=self.get_widget_records(player))
 			return data
+
+
+class RaceRankingsWidget(TimesWidgetView):
+	widget_x = -124.5
+	widget_y = 83.5
+	z_index = 130
+	size_x = 38
+	size_y = 55.5
+	top_entries = 5
+	title = 'Race Rankings'
+
+	template_name = 'live_rankings/widget.xml'
+
+	def __init__(self, app):
+		super().__init__(self)
+		self.app = app
+		self.manager = app.context.ui
+		self.id = 'pyplanet__widgets_racerankings'
+
+		self.record_amount = 15
+
+	def get_widget_records(self, player=None):
+		list_records = list()
+
+		player_index = len(self.app.current_finishes) + 1
+		if player:
+			player_record = [x for x in self.app.current_finishes if x['login'] == player.login]
+		else:
+			player_record = list()
+
+		if len(player_record) > 0:
+			# Set player index if there is a record
+			player_index = (self.app.current_finishes.index(player_record[0]) + 1)
+
+		records = list(self.app.current_finishes[:self.top_entries])
+		if self.app.instance.performance_mode:
+			# Performance mode is turned on, get the top of the whole widget.
+			records += self.app.current_finishes[self.top_entries:self.record_amount]
+			custom_start_index = (self.top_entries + 1)
+		else:
+			if player_index > len(self.app.current_finishes):
+				# No personal record, get the last records
+				records_start = (len(self.app.current_finishes) - self.record_amount + self.top_entries)
+				# If start of current slice is in the top entries, add more records below
+				if records_start < self.top_entries:
+					records_start = self.top_entries
+
+				records += list(self.app.current_finishes[records_start:])
+				custom_start_index = (records_start + 1)
+			else:
+				if player_index <= self.top_entries:
+					# Player record is in top X, get following records (top entries + 1 onwards)
+					records += self.app.current_finishes[self.top_entries:self.record_amount]
+					custom_start_index = (self.top_entries + 1)
+				else:
+					# Player record is not in top X, get records around player record
+					# Same amount above the record as below, except when not possible (favors above)
+					records_to_fill = (self.record_amount - self.top_entries)
+					start_point = ((player_index - math.ceil((records_to_fill - 1) / 2)) - 1)
+					end_point = ((player_index + math.floor((records_to_fill - 1) / 2)) - 1)
+
+					# If end of current slice is outside the list, add more records above
+					if end_point > len(self.app.current_finishes):
+						end_difference = (end_point - len(self.app.current_finishes))
+						start_point = (start_point - end_difference)
+					# If start of current slice is in the top entries, add more records below
+					if start_point < self.top_entries:
+						start_point = self.top_entries
+
+					records += self.app.current_finishes[start_point:(start_point + records_to_fill)]
+					custom_start_index = (start_point + 1)
+
+		index = 1
+		for record in records:
+			list_record = dict()
+			list_record['index'] = index
+			list_record['bgcolor'] = '00000070'
+
+			if player:
+				if player.flow.team_id == 0:
+					list_record['bgcolor'] = '7881F2FF'
+				if player.flow.team_id == 1:
+					list_record['bgcolor'] = 'F05F5FFF'
+
+			list_record['color'] = '$fff'
+			if index <= self.top_entries:
+				list_record['color'] = '$ff0'
+			if index == player_index:
+				list_record['color'] = '$0f3'
+
+			list_record['nickname'] = record['nickname']
+			list_record['score'] = times.format_time(int(record['score']))
+			list_record['cp_difference'] = None
+
+			# Points added on finishing in Rounds, Team or Cup modes.
+			if 'points_added' in record:
+				list_record['points_added'] = record['points_added']
+			else:
+				list_record['points_added'] = 0
+
+			if index == self.top_entries:
+				index = custom_start_index
+			else:
+				index += 1
+
+			list_records.append(list_record)
+		return list_records
+
+	async def get_context_data(self):
+		self.record_amount = await self.app.setting_rankings_amount.get_value()
+		if self.record_amount < 15:
+			self.record_amount = 15
+
+		data = await super().get_context_data()
+		if self.app.instance.performance_mode:
+			data['times'] = self.get_widget_records()
+		return data
+
+	async def get_player_data(self):
+		data = await super().get_player_data()
+
+		# If in performance mode, ignore this method.
+		if self.app.instance.performance_mode:
+			return dict()
+		else:
+			for player in self.app.instance.player_manager.online:
+				data[player.login] = dict(times=self.get_widget_records(player))
+			return data
