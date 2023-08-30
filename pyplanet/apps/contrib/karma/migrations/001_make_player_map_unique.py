076d072ef31f00ef4716100ba439aeea9fadc447
Tom Valk <tomvalk@lt-box.info>
2023-08-30 18:55:29 +0300

    Fixing the bug and inconsistancy with duplicated votes in the karma app. (MIGRATION ADDED). Removing duplicated records while migrating (FIX for multiple duplicates)

--- pyplanet/apps/contrib/karma/migrations/001_make_player_map_unique.py
+++ pyplanet/apps/contrib/karma/migrations/001_make_player_map_unique.py
@@ -9,18 +9,20 @@ logger = logging.getLogger(__name__)
 
 def upgrade(migrator: SchemaMigrator):
 	# Fix the duplicates in the current setup.
-	results = (Karma.select()
-			   .order_by(Karma.created_at.desc())
-			   .group_by(Karma.player_id, Karma.map_id)
-			   .having(fn.Count(Karma.player_id) > 1))
+	total_processed = 0
+	results = None
+	while results is None or len(results) > 0:
+		results = (Karma.select()
+				   .order_by(Karma.created_at.desc())
+				   .group_by(Karma.player_id, Karma.map_id)
+				   .having(fn.Count(Karma.player_id) > 1))
 
-	processed = 0
-	for result in results:
-		result.delete_instance()
-		processed += 1
+		for result in results:
+			result.delete_instance()
+			total_processed += 1
 
 	logger.warning('Removing duplicated karma votes, processed {} rows! Now going to execute the migration...'.format(
-		processed
+		total_processed
 	))
 
 	# Create the indexes.
